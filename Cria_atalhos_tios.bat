de15267dfb991389c551c5687fcbab5ea3125758
Tiago Gonzalez Strugulski <tiago_gonzalez40@hotmail.com>
2026-06-12 10:14:22 -0300

    Alertas de Execução

--- Cria_atalhos_tios.bat
+++ Cria_atalhos_tios.bat
@@ -5,9 +5,11 @@
 @echo off
 :: Altera a codificação do prompt para UTF-8 (corrige problemas com o acento de "Área de Trabalho")
 chcp 65001 > nul
-echo =========================================
-echo                Cria Atalhos Tios
-echo =========================================
+echo.
+echo ====================================================================
+echo                         Cria Atalhos Tios
+echo ====================================================================
+echo.
 :: ==============================================================================
 :: VALIDAÇÃO DE ADMINISTRADOR
 :: ==============================================================================
@@ -16,7 +18,9 @@ if %errorLevel% NEQ 0 (
     echo ====================================================================
     echo [ERRO] ESTE SCRIPT PRECISA SER EXECUTADO COMO ADMINISTRADOR!
     echo ====================================================================
+    echo.
     echo Por favor, clique com o botão direito no arquivo .bat e selecione:
+    echo.
     echo "Executar como Administrador"
     echo ====================================================================
     echo.
@@ -35,6 +39,7 @@ set "PastaTrabalho=C:\TMCTIOS\D2\D2A001W\20200304101\TIOS\bin"
 if not exist "%ExecutavelSistema%" (
     echo [ERRO] O executavel do sistema nao foi encontrado em:
     echo %ExecutavelSistema%
+    echo Verifique a instalação antes de Executar esse Script
     echo.
     pause
     exit
@@ -50,22 +55,25 @@ for /f "tokens=*" %%G in ('dir /b /ad "C:\Users"') do (
         if exist "C:\Users\%%G\Desktop" (
             set "DestinoFinal=C:\Users\%%G\Desktop\%NomeAtalho%"
             call :CriarAtalho
-            echo Atalho criado no Desktop de: %%G
+            echo   Atalho criado no Desktop de: %%G
+            echo.            
         )
         
         :: Caminho em Português
         if exist "C:\Users\%%G\Área de Trabalho" (
             set "DestinoFinal=C:\Users\%%G\Área de Trabalho\%NomeAtalho%"
             call :CriarAtalho
-            echo Atalho criado na Area de Trabalho de: %%G
+            echo   Atalho criado na Area de Trabalho de: %%G
+            echo.
         )
     )
 )
 
 echo.
-echo =========================================
-echo Processo concluido com sucesso!
-echo =========================================
+echo ====================================================================
+echo                 Processo concluido com sucesso!
+echo ====================================================================
+echo.
 pause
 exit
 
